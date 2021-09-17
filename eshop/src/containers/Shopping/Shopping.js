da390357746599abcc7ff95ae3be5ae042febf1d
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-09-17 22:33:37 +0430

    shopping-component

---
+++ eshop/src/containers/Shopping/Shopping.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Wrapper from "../../hoc/Wrapper";
+
+class Shopping extends React.Component {
+
+    render() {
+        return (
+            <Wrapper>
+                <div>controls</div>
+            </Wrapper>
+        )
+    }
+}
+
+export default Shopping
