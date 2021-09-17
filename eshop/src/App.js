18eab802a48bd0ebeafc4e6a974a5825ede4635f
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-09-17 18:06:53 +0430

    create_react-app eShop and delete extra file

---
+++ eshop/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component{
+    render(){
+        return(
+        <div>
+            <p>
+                hello world
+            </p>
+        </div>)
+    }
+}
+
+export default App;
